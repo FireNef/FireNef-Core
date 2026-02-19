4a76881324376ee670b6c59cdc1b71e9d5755e51
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2026-02-19 16:58:35 +0100

    Use spaced default name 'Basic Material'
    Change the BasicMaterialComponent constructor default from "BasicMaterial" to "Basic Material" for a clearer, more consistent display label. No functional behavior changed; only the default name passed to StandardMaterialComponent was updated.

--- javascript/components/3d/materials/basicMaterial.js
+++ javascript/components/3d/materials/basicMaterial.js
@@ -2,7 +2,7 @@ import { StandardMaterialComponent } from "./standardMaterial.js";
 import * as THREE from "three";
 
 export class BasicMaterialComponent extends StandardMaterialComponent {
-    constructor(name = "BasicMaterial") {
+    constructor(name = "Basic Material") {
         super(name, false, false, true, false, false, false, false, false);
 
         this.material = new THREE.MeshBasicMaterial();
