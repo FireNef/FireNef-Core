7384668bed8f9ef6cf61a76796a32c5fb69161e7
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2026-04-07 19:38:58 +0200

    Encapsulate Three.js geometries within GeometryComponent
    Introduces `GeometryComponent` to manage `THREE.Geometry` instances as first-class components.
    
    Mesh components now reference a `GeometryComponent` instead of directly holding a `THREE.Geometry` object. This promotes a more consistent component-based architecture and aligns geometry management with other component types like materials.

---
+++ javascript/components/3d/geometry.js
@@ -0,0 +1,31 @@
+import { Component } from "../component.js";
+import { Attribute } from "../attributes.js";
+
+export class GeometryComponent extends Component {
+    constructor(name = "Geometry") {
+        super(name);
+
+        const geometryAttribute = new Attribute("Geometry");
+        geometryAttribute.addField("Geometry", "three", null);
+        this.attributes.push(geometryAttribute);
+        
+        this.geometry = null;
+    }
+
+    start() {
+        this.updateAllProperties();
+    }
+
+    updateAllProperties() {
+        this.updateGeometry();
+    }
+
+    updateGeometry() {
+        this.geometry = this.getAttr("Geometry", "Geometry");
+    }
+
+    async setAttributeFieldValue(attribute, field, value, type, inputs = {}) {
+        await super.setAttributeFieldValue(attribute, field, value, type, inputs);
+        if (attribute == "Geometry") this.updateGeometry();
+    }
+}
