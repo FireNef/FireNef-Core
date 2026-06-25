5d154f0a60de3634a270f544da703d586f04ea05
Minecat <chrisdanriviere@gmail.com>
2026-06-25 18:18:51 +0200

    fixed bitmap 2d text component

--- javascript/components/2d/bitmapText2d.js
+++ javascript/components/2d/bitmapText2d.js
@@ -16,12 +16,17 @@ export class BitmapText2D extends Object2d {
 
         const formattingAttr = new Attribute("Formatting");
         formattingAttr.addField("Font Size", "number", 32, { min: 1, max: 500 });
-        formattingAttr.addField("Tint Color", "color", "#ffffff");
+        formattingAttr.addField("Fill Color", "color", "#ffffff");
         formattingAttr.addField("Align", "string", "left", {
             options: ["left", "center", "right"]
         });
         formattingAttr.addField("Letter Spacing", "number", 0);
         this.attributes.push(formattingAttr);
+
+        const wrapAttr = new Attribute("Word Wrap");
+        wrapAttr.addField("Enable Wrap", "boolean", false);
+        wrapAttr.addField("Wrap Width", "number", 300, { min: 10 });
+        this.attributes.push(wrapAttr);
 
         this.object2D = new PIXI.BitmapText({
             text: "000000",
@@ -55,11 +60,14 @@ export class BitmapText2D extends Object2d {
         const fontFamily = this.getAttr("Text Content", "Font Name");
 
         const fontSize = this.getAttr("Formatting", "Font Size");
-        const tintColor = this.getAttr("Formatting", "Tint Color");
+        const fillColor = this.getAttr("Formatting", "Fill Color");
         const align = this.getAttr("Formatting", "Align");
         const letterSpacing = this.getAttr("Formatting", "Letter Spacing");
 
-        const parsedTintColor = PIXI.Color.shared.setValue(tintColor).toNumber();
+        const wordWrap = this.getAttr("Word Wrap", "Enable Wrap");
+        const wordWrapWidth = this.getAttr("Word Wrap", "Wrap Width");
+
+        const parsedFillColor = PIXI.Color.shared.setValue(fillColor).toNumber();
 
         this.object2D.text = textString;
 
@@ -67,10 +75,11 @@ export class BitmapText2D extends Object2d {
             fontFamily: fontFamily,
             fontSize: fontSize,
             align: align,
-            letterSpacing: letterSpacing
+            letterSpacing: letterSpacing,
+            wordWrap: wordWrap,
+            wordWrapWidth: wordWrapWidth,
+            fill: parsedFillColor
         };
-
-        this.object2D.tint = parsedTintColor;
 
         const zIndex = this.getAttr("Text Content", "Z Index");
         if (this.object2D.zIndex !== zIndex) {
@@ -82,7 +91,7 @@ export class BitmapText2D extends Object2d {
     async setAttributeFieldValue(attribute, field, value, type) {
         await super.setAttributeFieldValue(attribute, field, value, type);
 
-        if (attribute === "Text Content" || attribute === "Formatting") {
+        if (attribute === "Text Content" || attribute === "Formatting" || attribute === "Word Wrap") {
             this.syncBitmapText();
         }
     }
